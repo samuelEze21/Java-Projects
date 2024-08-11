07e7551e6de9c71a3aa21f0c23a04adc458165b1
Flosamuel <nwabuezesamuel21@gmail.com>
2024-08-11 12:31:05 -0700

    keep drill going

--- gateOne/MenstrualApp.java
+++ gateOne/MenstrualApp.java
@@ -1,4 +1,6 @@
 import java.util.Scanner; 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 
 public class MenstrualApp {
@@ -8,27 +10,48 @@ public class MenstrualApp {
 
 	Scanner scanner = new Scanner(System.in); 
 
-
+	// import the date time formatter to get the localDate and dateTime formatter 
+	DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE; 
 	System.out.println("CHECK YOUR MENSTRUAL CIRCLE:");
 
-	System.out.println("Enter the first day of your last period:"); 
-	String firstDayPeriod = scanner.next(); 
+	System.out.println("Enter the first day of your period (YYYY-MM-DD):"); 
+	String firstDayPeriod = scanner.next();
+
+	LocalDate lastPeriodDate = LocalDate.parse(firstDayPeriod, formatter);
+
+	System.out.println("How long is your length of flow(flow duration) (enter in days): "); 
+	int flowDuration = scanner.nextInt(); 
+
+	System.out.println("How long is your period cycle(enter in days) "); 
+	int periodCycle = scanner.nextInt(); 
 
 
+	LocalDate nextPeriodDate = lastPeriodDate.plusDays(periodCycle);
 
+	LocalDate ovulationDate = nextPeriodDate.minusDays(14);
+
+	LocalDate bleedingEndDate = lastPeriodDate.plusDays(flowDuration -1);
+
+        LocalDate firstSafePeriodStart = bleedingEndDate.plusDays(1);
+        LocalDate firstSafePeriodEnd = ovulationDate.minusDays(5);
+
+        LocalDate secondSafePeriodStart = ovulationDate.plusDays(3);
+        LocalDate secondSafePeriodEnd = nextPeriodDate.minusDays(1);
+
+        LocalDate fertilePeriodStart = ovulationDate.minusDays(5);
+        LocalDate fertilePeriodEnd = ovulationDate.plusDays(1);
+
+	System.out.println("======================================================================");
+        System.out.println("Your next period is expected on: " + nextPeriodDate.format(formatter));
+        System.out.println("Your ovulation date is: " + ovulationDate.format(formatter));
+        System.out.println("Your bleeding end date is from: "  + bleedingEndDate.format(formatter));
+        System.out.println("Your first safe period is from: " + firstSafePeriodStart.format(formatter) + " to " + firstSafePeriodEnd.format(formatter));
+        System.out.println("Your fertile period is from: " + fertilePeriodStart.format(formatter) + " to " + fertilePeriodEnd.format(formatter));
+        System.out.println("Your second safe period is from: " + secondSafePeriodStart.format(formatter) + " to " + secondSafePeriodEnd.format(formatter));
+
+
+    }
 }
-
-}
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -43,9 +66,9 @@ psudo:
 
 2. Prompt the user to enter the first day of their period 
 
-//3. prompt user to enter their menstrual circle
+3. prompt user to enter their menstrual circle
 
-//4. prompt user for the flow duration
+4. prompt user for the flow duration
 
 
 
---
+++ gateOne/PhoneBookApp.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+	public class PhoneBookApp {
+	  public static void main(String [] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+----------------
+
+top:
+
+create an app that will help miracle manage his contact, and the app must have the following; 
+
+1. add contact
+2. remove contact
+3. find contact by phone number
+4. find contacbyt by first name
+5. find contacbyt by last name
+6. edit contact
+
+
+----
+
+refinement:
+
+1. 
+
